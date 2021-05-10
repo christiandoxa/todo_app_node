cbca1a4d347bb99e6c0f7c54491712d2bd86cffa
Christian Doxa Hamasiah <zhergiuz@gmail.com>
2021-05-10 08:38:35 +0700

    initial commit

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
+const http = require("http");
+const indexRouter = require('./routes/index');
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+// error handler
+app.use('*', function (req, res, _) {
+    res.json({status: 404, message: "Not found"})
+});
+
+if (cluster.isMaster) {
+    console.log(`Primary ${process.pid} is running`);
+    // Fork workers.
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
+    cluster.on('exit', (worker) => {
+        console.log(`worker ${worker.process.pid} died`);
+    });
+} else {
+    // Workers can share any TCP connection
+    // In this case it is an HTTP server
+    const port = process.env.USAHAQ_PORT ?? 3000;
+    http.createServer(app).listen(3000);
+    console.log(`Worker ${process.pid} started`);
+}
