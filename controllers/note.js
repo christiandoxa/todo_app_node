2710fdd8d1af450255dbccad3e1d2c57361954dd
Christian Doxa Hamasiah <zhergiuz@gmail.com>
2021-05-10 11:55:48 +0700

    - get all note - get note by id

--- controllers/note.js
+++ controllers/note.js
@@ -29,3 +29,41 @@ exports.createNote = async function (req, res) {
         return commonErrorResponse(e, res);
     }
 };
+
+exports.getAllNotes = async function (req, res) {
+    try {
+        const {ID_ACCOUNT} = req.user;
+        const notes = await NOTE.findAll({where: {ID_ACCOUNT}});
+        return res.status(200).json({
+            status: 200,
+            notes
+        });
+    } catch (e) {
+        return commonErrorResponse(e, res);
+    }
+};
+
+exports.getNote = async function (req, res) {
+    try {
+        const {id_note} = req.params;
+        if (!id_note) {
+            return res.status(400).json({
+                status: 400,
+                message: "id note required"
+            });
+        }
+        const note = await NOTE.findOne({where: {ID_NOTE: id_note}});
+        if (note) {
+            return res.status(200).json({
+                status: 200,
+                note
+            });
+        }
+        return res.status(404).json({
+            status: 404,
+            message: "Can't find note with that id"
+        });
+    } catch (e) {
+        return commonErrorResponse(e, res);
+    }
+};
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const {
     account: {createAccount, signIn},
-    note: {createNote}
+    note: {createNote, getAllNotes, getNote}
 } = require('../controllers');
 const {jwtCheck: {checkToken}} = require('../middlewares');
 const router = express.Router();
 
+// account route
 router.post('/create_account', createAccount);
 router.post('/sign_in', signIn);
+
+
+// note route
+router.get('/notes', checkToken, getAllNotes);
+router.get('/note/:id_note', checkToken, getNote)
 router.post('/add_note', checkToken, createNote);
 
 module.exports = router;
